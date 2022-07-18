96cae3a590bd5ffa249163b317eda8817150be8a
Ramiro <ramiro_gear@hotmail.com>
2022-07-18 11:49:54 -0500

    Primer commit

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,39 @@
+import './mylabel.css'
+
+interface Props{
+  /**
+   * Este es el mensaje a mostrar en la etiqueta
+   */
+  label?: string;
+  /**
+   * Este es el tamaño de la etiqueta
+   */
+  size?: 'normal'|'h1'|'h2'|'h3';
+  /**
+   * Para saber si la etiqueta es en mayusculas
+   */
+   allCaps?: boolean;
+   /**
+   * Color de la etiqueta
+   */
+    color?: 'primary'|'secondary'|'tertiary';
+    /**
+   * Color personlizado de la fuente
+   */
+     fontColor?: string;
+}
+
+export const MyLabel = ( {
+  label = 'No label',
+  size = 'normal',
+  allCaps = false,
+  color = 'primary',
+  fontColor
+}: Props) => {
+  return (
+    <span className={ `label ${ size } text-${ color }` }
+      style={{ color: fontColor }}>
+        { allCaps ? label.toUpperCase() : label }
+    </span>
+  )
+}
